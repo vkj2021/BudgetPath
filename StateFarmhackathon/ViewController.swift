a1cb3dd89135cf5cc773254a03c7d35ae263a16b
vkj <vkj2021@gmail.com>
2022-04-03 08:29:20 -0500

    finishproduct

--- StateFarmhackathon/ViewController.swift
+++ StateFarmhackathon/ViewController.swift
@@ -7,13 +7,60 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func didTapbutton() {
+        present(ViewControllerHomePage(), animated: true)
+        let vc = storyboard?.instantiateViewController(identifier: "homepage_vc") as! HomePageViewController
+        present(vc, animated: true)
+    }
 
+}
+
+class ViewControllerHomePage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func didTapbutton() {
+        present(ViewControllerExpenses(), animated:  true)
+        let vc = storyboard?.instantiateViewController(identifier: "expenses_vc") as! ExpensesViewController
+        present(vc, animated: true)
+    }
+
+
+}
+
+class ViewControllerExpenses: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func didTapbutton() {
+        
+    present(ViewControllerVehicle(), animated: true)
+    let vc = storyboard?.instantiateViewController(identifier: "vehicle_vc") as! ViewControllerVehicle
+    present(vc, animated: true)
+
+
+    }
+    
+}
+
+
+class ViewControllerVehicle: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
 
 }
 
